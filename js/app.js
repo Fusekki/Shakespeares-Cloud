7b5b8c3318958eedd42ac4c6f56a8b45d94b40a3
grimal <phillip@philliprstafford.com>
2017-04-24 09:57:04 -0400

    Replaced filter definition in app.js

--- js/app.js
+++ js/app.js
@@ -58,15 +58,6 @@
         return $sce.trustAsHtml;
     });
 
-    // shakespeareApp.filter('highlight', function($sce) {
-    //     return function(text, phrase) {
-    //         if (phrase) text = text.replace(new RegExp('('+phrase+')', 'gi'),
-    //             '<span class="highlighted">$1</span>')
-    //
-    //         return $sce.trustAsHtml(text)
-    //     }
-    // });
-
 
 })();
 
