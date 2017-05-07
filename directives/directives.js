440cabde9dc134dcc5dac5761a792381f16d29e1
Phillip Stafford <phillip@philliprstafford.com>
2017-05-07 11:53:34 -0400

    Add custom 'mydir' directive to monitor element's changes.

--- directives/directives.js
+++ directives/directives.js
@@ -14,3 +14,41 @@ angular.module('shakespeareApp')
             }
         };
     })
+
+    .directive("mydir", function () {
+        return {
+            link: function (scope, ele, attrs) {
+                console.log("Inside link function");
+                console.log(ele);
+                scope.$watch(ele, function(newValue, oldValue) {
+                    console.log(oldValue);
+                        if (newValue !== oldValue) {
+                            console.log(newValue);
+
+                        }
+                });
+                // console.log(ele.nextElementSibling.previousElementSibling);?
+
+                // function () { return ele[0].childNodes.length; },
+                // function (newValue, oldValue) {
+                //     if (newValue !== oldValue) {
+                //         // code goes here
+                //     }
+                }
+                // console.log(ele);
+                //
+                // console.log(ele.nextSibling);
+                //
+                // // var movable = ele.find('.tooltip');
+                // if (ele.nextSibling) {
+                //     ele.addClass('dark')
+                }
+                // console.log(movable);
+                // scope.$watch("model.input", function (value) {
+                //     if(value === "password") {
+                //         element.children(1).toggleClass("alert-box alert");
+                //     }
+                // });
+            // }
+        }
+    )
