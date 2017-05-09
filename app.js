a7afa138f3f22a456c0f96c24c5fa51267af3e56
grimal <phillip@philliprstafford.com>
2017-05-09 11:27:05 -0400

    Move filter out of app.js and place in folder titler filters with a filename of filter.js.  This is done according to the Angular Best Practices.

--- app.js
+++ app.js
@@ -52,16 +52,6 @@
 
     });
 
-
-    shakespeareApp.filter('unsafe', ['$sce', function($sce){
-        return function(val) {
-            if (typeof(val) != 'string') {
-                val = val.toString();
-            }
-            return $sce.trustAsHtml(val);
-        };
-    }]);
-
 })();
 
 
