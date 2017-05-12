53459eb6390d9fc5d89407d9f138729a645d0f90
Phillip Stafford <phillip@philliprstafford.com>
2017-05-12 15:34:44 -0400

    Add line to find <floaralcap> elements and replace with nothing.  Find replacement text of <div-"line"> and replace with angular markup.

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -1,176 +1,144 @@
 module.exports = function (grunt) {
-  grunt.loadNpmTasks('grunt-replace');
-  grunt.initConfig({
-    replace: {
-      dist: {
-        options: {
-          patterns: [
-            {
-              match: /(<\?xml).+/,
-              replacement: ''
-            },
-            {
-              match: /(<\?xml-).+/,
-              replacement: ''
-            },
-                        {
-              match:/\">.+<\/title>/,
-              replacement: '</div>'
-            },
-            {
-              match: /(<play).+/,
-              replacement: '<div class="play">'
-            },
-                        {
-              match:/(<\/play.+)/g,
-              replacement: '</div>'
-            },
-                        {
-              match:/<title short="/,
-              replacement: '<div class="title">'
-            },
-                                               {
-              match:/<playwright>/g,
-              replacement: '<div class="playwright">'
-            },
-                                                           {
-              match:/<\/playwright>/g,
-              replacement: '</div>'
-            },
-                                                           {
-              match:/<edition>/g,
-              replacement: '<div class="edition">'
-            },
-                                                           {
-              match:/<\/edition>/g,
-              replacement: '</div>'
-            },
-                                                                      {
-              match:/<act>/g,
-              replacement: '<div class="act">'
-            },
-                                                                      {
-              match:/<\/act>/g,
-              replacement: '</div>'
-            },
-                                                                                  {
-              match:/<acttitle>/g,
-              replacement: '<div class="acttitle">'
-            },
-                                                                      {
-              match:/<\/acttitle>/g,
-              replacement: '</div>'
-            },
-                                                                                  {
-              match:/<scene>/g,
-              replacement: '<div class="scene">'
-            },
-                                                                      {
-              match:/<\/scene>/g,
-              replacement: '</div>'
-            },
-                                         {
-              match:/<scenetitle type=.+/g,
-              replacement: ''
-            },
+	grunt.loadNpmTasks('grunt-replace');
+	grunt.initConfig({
+		replace: {
+			dist: {
+				options: {
+					patterns: [{
+							match: /(<\?xml).+/,
+							replacement: ''
+						}, {
+							match: /(<\?xml-).+/,
+							replacement: ''
+						}, {
+							match: /\">.+<\/title>/,
+							replacement: '</div>'
+						}, {
+							match: /(<play).+/,
+							replacement: '<div class="play">'
+						}, {
+							match: /(<\/play.+)/g,
+							replacement: '</div>'
+						}, {
+							match: /<dropcap type=".+">/g,
+							replacement: ''
+						}, {
+							match: /<\/dropcap>/g,
+							replacement: ''
+						}, {
+							match: /<title short="/,
+							replacement: '<div class="title">'
+						}, {
+							match: /<playwright>/g,
+							replacement: '<div class="playwright">'
+						}, {
+							match: /<\/playwright>/g,
+							replacement: '</div>'
+						}, {
+							match: /<edition>/g,
+							replacement: '<div class="edition">'
+						}, {
+							match: /<\/edition>/g,
+							replacement: '</div>'
+						}, {
+							match: /<act>/g,
+							replacement: '<div class="act">'
+						}, {
+							match: /<\/act>/g,
+							replacement: '</div>'
+						}, {
+							match: /<acttitle>/g,
+							replacement: '<div class="acttitle">'
+						}, {
+							match: /<\/acttitle>/g,
+							replacement: '</div>'
+						}, {
+							match: /<scene>/g,
+							replacement: '<div class="scene">'
+						}, {
+							match: /<\/scene>/g,
+							replacement: '</div>'
+						}, {
+							match: /<scenetitle type=.+/g,
+							replacement: ''
+						},
+						{
+							match: /<stagedir>/g,
+							replacement: '<div class="stagedir" change-on-tooltip tooltip-append-to-body="false"  tooltip-trigger="&#39;outsideClick&#39;" tooltip-placement="top" tooltip-class="tooltipClass" uib-tooltip-template="&#39;templates/tooltip.tmpl.htm&#39;">'
+						}, 
+						{
+							match: /<\/stagedir>/g,
+							replacement: '</div>'
+						}, {
+							match: /<speech>/g,
+							replacement: '<div class="speech">'
+						}, {
+							match: /<\/speech>/g,
+							replacement: '</div>'
+						}, {
+							match: /<speaker>/g,
+							replacement: '<div class="speaker">'
+						}, {
+							match: /<\/speaker>/g,
+							replacement: '</div>'
+						}, {
+							match: /<line order="\w+">/g,
+							replacement: '<div class="line" change-on-tooltip tooltip-append-to-body="false"  tooltip-trigger="&#39;outsideClick&#39;" tooltip-placement="top" tooltip-class="tooltipClass" uib-tooltip-template="&#39;templates/tooltip.tmpl.htm&#39;">'
+						}, {
+							match: /<\/line>/,
+							replacement: '</div>'
+						}, {
+							match: /<line>/g,
+							replacement: '<div class="line" change-on-tooltip tooltip-append-to-body="false"  tooltip-trigger="&#39;outsideClick&#39;" tooltip-placement="top" tooltip-class="tooltipClass" uib-tooltip-template="&#39;templates/tooltip.tmpl.htm&#39;">'
+						}, {
+							match: /<\/line>/g,
+							replacement: '</div>'
+						}, {
+							match: /<finis>/g,
+							replacement: '<div class="finis">'
+						}, {
+							match: /<\/finis>/g,
+							replacement: '</div>'
+						}, {
+							match: /<epilogue>/g,
+							replacement: '<div class="epilogue">'
+						}, {
+							match: /<\/epilogue>/g,
+							replacement: '</div>'
+						}, {
+							match: /<sourcedetails>/g,
+							replacement: '<!-- <sourcedetails>'
+						}, {
+							match: /<\/sourcedetails>/g,
+							replacement: '</sourcedetails> -->'
+						}, {
+							match: /^\s*\n/gm,
+							replacement: ''
+						}, {
+							match: /<finistitle>/g,
+							replacement: ''
+						}, {
+							match: /<\/finistitle>/g,
+							replacement: ''
+						}
+					]
+				},
+				files: [{
+						expand: true,
+						flatten: true,
+						src: ['xml/*.xml'],
+						dest: 'xml/build/',
+						rename: function (dest, src) {
+							return dest + src.replace(/\.xml$/, ".htm");
+						}
 
-                                                                                                        {
-              match:/<stagedir>/g,
-			  replacement: '<div class="stagedir" change-on-tooltip tooltip-append-to-body="false"  tooltip-trigger="&#39;outsideClick&#39;" tooltip-placement="top" tooltip-class="tooltipClass" uib-tooltip-template="&#39;templates/tooltip.tmpl.htm&#39;">'
-
-            },
-                                                                      {
-              match:/<\/stagedir>/g,
-              replacement: '</div>'
-            },
-                                                                                                                  {
-              match:/<speech>/g,
-              replacement: '<div class="speech">'
-            },
-                                                                      {
-              match:/<\/speech>/g,
-              replacement: '</div>'
-            },
-                                                                                                                  {
-              match:/<speaker>/g,
-              replacement: '<div class="speaker">'
-            },
-                                                                      {
-              match:/<\/speaker>/g,
-              replacement: '</div>'
-            },
-                                                                                                                           {
-              match:/<line order="\w+">/g,
-              replacement: '<div class="line">'
-            },
-                                                                      {
-              match:/<\/line>/,
-              replacement: '</div>'
-            },
-                                                                                                                              {
-              match:/<line>/g,
-              replacement: '<div class="line" change-on-tooltip tooltip-append-to-body="false"  tooltip-trigger="&#39;outsideClick&#39;" tooltip-placement="top" tooltip-class="tooltipClass" uib-tooltip-template="&#39;templates/tooltip.tmpl.htm&#39;">'
-            },
-                                                                      {
-              match:/<\/line>/g,
-              replacement: '</div>'
-            },
-                                                                                                                                          {
-              match:/<finis>/g,
-              replacement: '<div class="finis">'
-            },
-                                                                      {
-              match:/<\/finis>/g,
-              replacement: '</div>'
-            },
-                                                                                                                                                      {
-              match:/<epilogue>/g,
-              replacement: '<div class="epilogue">'
-            },
-                                                                      {
-              match:/<\/epilogue>/g,
-              replacement: '</div>'
-            },
-                                                                                                                                                                  {
-              match:/<sourcedetails>/g,
-              replacement: '<!-- <sourcedetails>'
-            },
-                                                                      {
-              match:/<\/sourcedetails>/g,
-              replacement: '</sourcedetails> -->'
-            },
-                                                                                  {
-              match:/^\s*\n/gm,
-              replacement: ''
-            },
-                                                                                              {
-              match:/<finistitle>/g,
-              replacement: ''
-            },
-                                                                                              {
-              match:/<\/finistitle>/g,
-              replacement: ''
-            }
-          ]
-        },
-        files: [
-          {
-            expand: true,
-            flatten: true,
-            src: ['xml/*.xml'],
-            dest: 'xml/build/',
-             rename: function(dest, src) {
-          return dest + src.replace(/\.xml$/, ".htm");
-        }
-
-          }
-        ]
-      }
-    }
-  });
-  grunt.registerTask('default', 'replace');
+					}
+				]
+			}
+		}
+	});
+	grunt.registerTask('default', 'replace');
 };
-
 
 // Replace items
 // <?xml ... ?> with ""
